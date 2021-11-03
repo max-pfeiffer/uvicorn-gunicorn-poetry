0a301df12a286ea670c3979b9f708d7342a320ba
Max Pfeiffer <max.pfeiffer@alp-phone.ch>
2021-11-03 16:21:32 +0100

    Added a test entrypoint for building docker executables for running tests. Updated the sample project with an additional unit-test-image for demonstrating and testing this approach.

--- tests/functionality/test_running_test_executables.py
+++ tests/functionality/test_running_test_executables.py
@@ -1,4 +1,5 @@
 import time
+from typing import Dict
 
 from build.images import UvicornGunicornPoetryImage, FastApiMultistageImage
 from tests.constants import TEST_CONTAINER_NAME, SLEEP_TIME
@@ -10,22 +11,11 @@ def test_worker_reload(docker_client) -> None:
     UvicornGunicornPoetryImage(docker_client) \
         .build('python3.8.12-slim-bullseye')
     test_image: Image = FastApiMultistageImage(docker_client) \
-        .build('python3.8.12-slim-bullseye', 'development-image')
+        .build('python3.8.12-slim-bullseye', 'unit-test-image')
 
-    test_container: Container = \
+    api_response: Dict = \
         docker_client.containers.run(test_image.tags[0],
                                      name=TEST_CONTAINER_NAME,
                                      ports={"80": "8000"},
-                                     detach=True)
-    time.sleep(SLEEP_TIME)
-
-    (exit_code, output) = test_container.exec_run([
-        'touch',
-        '/application_root/app/main.py'
-    ])
-    assert exit_code == 0
-    assert output.decode("utf-8") == ''
-    time.sleep(SLEEP_TIME)
-
-    logs: str = test_container.logs().decode("utf-8")
-    assert 'Worker reloading: /application_root/app/main.py modified' in logs
+                                     detach=True).wait()
+    assert api_response['StatusCode'] == 0
