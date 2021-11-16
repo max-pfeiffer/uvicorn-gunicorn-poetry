2134f4ae70f47e7a40fb076bd6bcddb0c3860287
Max Pfeiffer <max.pfeiffer@alp-phone.ch>
2021-11-16 21:58:29 +0100

    Added some more prep and cleanup for tests. Added a RUN command after COPY from.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import docker
 from docker.errors import NotFound
 
+from build.images import UvicornGunicornPoetryImage, FastApiMultistageImage
 from tests.constants import TEST_CONTAINER_NAME
 
 
@@ -12,16 +13,54 @@ def docker_client() -> docker.client:
 
 @pytest.fixture(autouse=True)
 def prepare_docker_env(docker_client) -> None:
+    # Remove old container
     try:
         old_container = docker_client.containers.get(TEST_CONTAINER_NAME)
         old_container.stop()
         old_container.remove()
     except NotFound:
         pass
+    # Delete old existing images
+    uvicorn_gunicorn_poetry_image: UvicornGunicornPoetryImage = (
+        UvicornGunicornPoetryImage(docker_client)
+    )
+    for old_image in docker_client.images.list(
+            uvicorn_gunicorn_poetry_image.image_name
+    ):
+        for tag in old_image.tags:
+            docker_client.images.remove(tag, force=True)
+    fast_api_multistage_image: FastApiMultistageImage = (
+        FastApiMultistageImage(docker_client)
+    )
+    for old_image in docker_client.images.list(
+            fast_api_multistage_image.image_name
+    ):
+        for tag in old_image.tags:
+            docker_client.images.remove(tag, force=True)
+
     yield None
+
+    # Remove old container
     try:
         old_container = docker_client.containers.get(TEST_CONTAINER_NAME)
         old_container.stop()
         old_container.remove()
     except NotFound:
         pass
+    # Delete old existing images
+    uvicorn_gunicorn_poetry_image: UvicornGunicornPoetryImage = (
+        UvicornGunicornPoetryImage(docker_client)
+    )
+    for old_image in docker_client.images.list(
+            uvicorn_gunicorn_poetry_image.image_name
+    ):
+        for tag in old_image.tags:
+            docker_client.images.remove(tag, force=True)
+    fast_api_multistage_image: FastApiMultistageImage = (
+        FastApiMultistageImage(docker_client)
+    )
+    for old_image in docker_client.images.list(
+            fast_api_multistage_image.image_name
+    ):
+        for tag in old_image.tags:
+            docker_client.images.remove(tag, force=True)
