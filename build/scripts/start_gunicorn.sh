83ce552b2a86c0f714b9fb47a15f11f5dbaa4d87
Max Pfeiffer <max@maxpfeiffer.ch>
2023-02-28 19:54:56 +0100

    Refactored publish script location,updated GitHub workflow
    Publish script is now placed in build module, pipeline jobs are now chained properly.

--- build/scripts/start_gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Activating virtual environment..."
-. /application_root/.venv/bin/activate
-
-# Start Gunicorn
-exec gunicorn -k uvicorn.workers.UvicornWorker -c /application_server/gunicorn_configuration.py app.main:app
